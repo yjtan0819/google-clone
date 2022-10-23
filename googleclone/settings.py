c9452a9dbd2a918b2a3773c775beaaea3e5a9dba
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-10-23 00:26:30 -0400

    Updated settings.py

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os, django_heroku, dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--ew4%5490s@c-sfpl*ycq17ww=zw*#-fguozw9&w-@w$z&#=1u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
