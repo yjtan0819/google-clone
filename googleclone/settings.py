48bfd64df38b896d766fa138d64115da6c41976a
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-10-23 00:37:56 -0400

    Updated requirements.txt

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure--ew4%5490s@c-sfpl*ycq17ww=zw*#-fguozw9&w-@w$z&#=1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
