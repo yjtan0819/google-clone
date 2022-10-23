28c1e0b12dee54a71080e2f66b99961dba684906
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-10-23 00:40:45 -0400

    Modified settings.py

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ew4%5490s@c-sfpl*ycq17ww=zw*#-fguozw9&w-@w$z&#=1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
